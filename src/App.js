3ba9a829faca78c42e0521deef72b54a19c51dcc
PughalBot <77720496+PughalBot@users.noreply.github.com>
2023-02-06 19:01:20 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+
+import './styles.css';
+
+// import video
+import videoBg from '../src/assets/bg.mp4';
+import Logo from '../src/assets/lol.png';
+
+const App = () => {
+  return (
+    <section className='page'>
+      {/* overlay */}
+      <div className='overlay'></div>
+      <video src={videoBg} autoPlay loop muted />
+      <div className='page__content'>
+        <img className='logo' src={Logo}/>
+        <h1>Launching Soon</h1>
+        <h3>
+          Official Ebiv Website Will be available soon
+        </h3>
+        <FlipClockCountdown
+          className='flip-clock'
+          to={new Date().getTime() + 600 * 3600 * 1000 + 5000}
+        />
+        <button className='btn'>Soon!!</button>
+      </div>
+    </section>
+  );
+};
+
+export default App;
